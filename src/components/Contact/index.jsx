2ba4fb95e3323947a4b2463a20e7d57445a3ad2f
Andreea-Lita <litaandreeamalaiasi@gmail.com>
2024-03-31 21:28:12 +0100

    am decomentat import avatar din more about me & contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,28 +3,20 @@ import './style.css'
 import Container from '../Container';
 import Row from '../Row';
 import Col from '../Col';
-import EarthPlanet from '../Images/shot.jpg';
+
 import Avatar from '../Images/2151100212.jpg'
 
 function Contact() {
   return (
     <div style={{ marginTop: 30, marginLeft: 200}}>
       
-        {/* <h1></h1>
-        <h2></h2> */}
+       
         <span class="badge badge-pill badge-secondary"><a href="https://github.com/Andreea-Lita">GitHub</a></span>
         <br />
         <span class="badge badge-pill badge-primary"><a href="https://www.linkedin.com/in/andreea-malaiasi-bb15aa235">Linkedin</a></span>
         <br />
         <span class="badge badge-pill badge-secondary"><a href="https://mail.google.com/">myemail</a></span>
-        {/* <a href="https://www.linkedin.com/in/andreea-malaiasi-bb15aa235">Linkedin</a>
-        <br />
-        <a href="https://github.com/Andreea-Lita">GitHub</a>*/}
-        <br />
-        {/* <a href="https://mail.google.com/">myemail</a>  */}
-        {/* <p>Tel: </p> */}
-        
-        {/* <Container style={{ marginTop: 30 }}> */}
+     
       <Container style={{ marginTop: 90 }}>
         <Row>
           <Col size="md-8">
